d18a499c4f2ed741be70cd4451400813c8f61b4a
planethouki <26595148+planethouki@users.noreply.github.com>
2019-06-10 22:47:33 +0900

    today ionic

--- ionic-firebase-1/ionic-board/src/app/home/home.page.ts
+++ ionic-firebase-1/ionic-board/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,72 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  message: string;
+
+  post: {
+    userName: string,
+    message: string,
+    createDate: any
+  } = {
+    userName: 'Taro Yamada',
+    message: 'これはテストメッセージです',
+    createDate: '10分前'
+  };
+
+  posts: { userName: string, message: string, createDate: any }[] = [
+    {
+      userName: 'Taro Yamada',
+      message: 'これはテストメッセージです',
+      createDate: '10分前'
+    },
+    {
+      userName: 'Taro Yamada',
+      message: 'これはテストメッセージです',
+      createDate: '10分前'
+    }
+  ];
+
+  constructor(private alertCtrl: AlertController) {}
+
+  addPost() {
+    this.post = {
+      userName: 'Akira Yanagihara',
+      message: this.message,
+      createDate: '数秒前'
+    };
+
+    this.posts.push(this.post);
+    this.message = '';
+  }
+
+  async presentPrompt(index: number) {
+    const alert = await this.alertCtrl.create({
+      header: 'メッセージ編集',
+      inputs: [
+        {
+          name: 'message',
+          type: 'text',
+          placeholder: 'メッセージ'
+        }
+      ],
+      buttons: [
+        {
+          text: 'キャンセル',
+          role: 'cancel',
+          handler: () => {
+            console.log('キャンセルが選択されました');
+          }
+        },
+        {
+          text: '更新',
+          handler: data => {
+            console.log(data);
+            this.posts[index].message = data.message;
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
 }
