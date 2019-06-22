6b8005bf28af204d0030dab08f451adde4f435c0
planethouki <26595148+planethouki@users.noreply.github.com>
2019-06-22 20:55:59 +0900

    contentful

---
+++ contetntful/smaple1/nuxt.config.js
@@ -0,0 +1,78 @@
+import colors from 'vuetify/es5/util/colors'
+const config = require('./.contentful.json')
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    theme: {
+      primary: colors.blue.darken2,
+      accent: colors.grey.darken3,
+      secondary: colors.amber.darken3,
+      info: colors.teal.lighten1,
+      warning: colors.amber.base,
+      error: colors.deepOrange.accent4,
+      success: colors.green.accent3
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  },
+
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  }
+}
---
+++ contetntful/smaple1/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- render data of the person -->
+    <h1>{{ person.fields.name }}</h1>
+    <!-- render blog posts -->
+    <ul>
+      <li v-for="post in posts">
+        {{ post.fields.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {createClient} from '~/plugins/contentful.js'
+
+const client = createClient()
+
+export default {
+  // `env` is available in the context object
+  asyncData ({env}) {
+    return Promise.all([
+      // fetch the owner of the blog
+      client.getEntries({
+        'sys.id': env.CTF_PERSON_ID
+      }),
+      // fetch all blog posts sorted by creation date
+      client.getEntries({
+        'content_type': env.CTF_BLOG_POST_TYPE_ID,
+        order: '-sys.createdAt'
+      })
+    ]).then(([entries, posts]) => {
+      // return data that should be available
+      // in the template
+      return {
+        person: entries.items[0],
+        posts: posts.items
+      }
+    }).catch(console.error)
+  }
+}
+</script>
