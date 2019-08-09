5c7290c81f1461394fb4a60efd2b592d7259abae
planethouki <26595148+planethouki@users.noreply.github.com>
2019-08-09 22:02:45 +0900

    sha3

--- nem2-sdk/sha3.js
+++ nem2-sdk/sha3.js
@@ -3,7 +3,8 @@ const jssha3 = require('js-sha3');
 const sha3_256 = jssha3.sha3_256;
 
 const payload = process.argv[2];
+console.log(payload);
 const hasher = sha3_256.create();
 const hash = hasher.update(Buffer.from(payload, 'hex')).hex().toUpperCase();
 
-console.log('hash :' + hash);
+console.log('hash: ' + hash);
