c1a0371f9e4072cd11466cb4432b0327457b6493
planethouki <26595148+planethouki@users.noreply.github.com>
2020-04-25 15:58:44 +0900

    sym sdk 0.18.0

---
+++ symbol-sdk-0.18.0/account.js
@@ -0,0 +1,13 @@
+const { 
+    Account,
+    NetworkType
+} = require('nem2-sdk');
+
+const networkType = NetworkType.TEST_NET
+
+const initiator = Account.createFromPrivateKey(
+    '25B3F54217340F7061D02676C4B928ADB4395EB70A2A52D2A11E2F4AE011B03E',
+    networkType
+)
+
+console.log(initiator.address.plain());
