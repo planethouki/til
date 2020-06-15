967308ec92233e46c31cabf02bbf62125dd2218b
planethouki <26595148+planethouki@users.noreply.github.com>
2020-06-15 22:09:50 +0900

    nem

--- symbol-sdk-0.18.0/account.js
+++ symbol-sdk-0.18.0/account.js
@@ -5,10 +5,13 @@ const {
 
 const networkType = NetworkType.TEST_NET
 
-const initiator = Account.createFromPrivateKey(
-    '25B3F54217340F7061D02676C4B928ADB4395EB70A2A52D2A11E2F4AE011B03E',
-    networkType
-)
+// const initiator = Account.createFromPrivateKey(
+//     '25B3F54217340F7061D02676C4B928ADB4395EB70A2A52D2A11E2F4AE011B03E',
+//     networkType
+// )
 
-console.log(initiator.address.plain());
-console.log(initiator.publicAccount.publicKey);
+const initiator = Account.generateNewAccount(networkType)
+
+console.log(initiator.privateKey)
+console.log(initiator.address.plain())
+console.log(initiator.publicAccount.publicKey)
