9fef2ef91dbc72e23042d8da87b8a451574ba5ec
planethouki <26595148+planethouki@users.noreply.github.com>
2020-08-19 21:48:41 +0900

    commit

--- symbol-sdk-0.20.7/accountDistBackRandom.js
+++ symbol-sdk-0.20.7/accountDistBackRandom.js
@@ -6,8 +6,9 @@
         return Math.floor(Math.random() * (max - min)) + min;
     }
     while (true) {
-        const from = getRandomInt(0xFFFF, 0xFFFFFFFF);
+        const from = getRandomInt(0xFFFF, 0xFFFFFF);
         const to = from + increment;
+        console.log(from, to);
         await require('./accountIncrease').dist(from, to);
         await require('./accountIncrease').back(from, to);
     }
