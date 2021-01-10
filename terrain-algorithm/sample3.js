e008ff60b5e9782d565c024ba1c9b3df0e4a8101
planethouki <26595148+planethouki@users.noreply.github.com>
2021-01-10 23:59:35 +0900

    square diamond

--- terrain-algorithm/sample3.js
+++ terrain-algorithm/sample3.js
@@ -4,9 +4,11 @@
 const PNG = require("pngjs").PNG;
 const fs = require("fs");
 
-const power = 9;
-const nx = Math.pow(2, power) + 1;
-const ny = Math.pow(2, power) + 1;
+const n = 9;
+const max = Math.pow(2, n);
+
+const nx = max + 1;
+const ny = max + 1;
 
 const newfile = new PNG({ width: nx, height: ny });
 
@@ -27,6 +29,7 @@ function getRandomArbitrary(min, max) {
  * @param {number} value 0 to 1
  */
 function setValue(file, x, y, value) {
+  if (x < 0 || y < 0 || x > max || y > max) { return; }
   let idx = (file.width * y + x) << 2;
   file.data[idx + 0] = value * 0xff;
   file.data[idx + 1] = value * 0xff;
@@ -35,81 +38,62 @@ function setValue(file, x, y, value) {
 }
 
 function getValue(file, x, y) {
+  if (x < 0 || y < 0 || x > max || y > max) { return -1; }
   let idx = (file.width * y + x) << 2;
   return (file.data[idx + 0] + file.data[idx + 1] + file.data[idx + 2]) / (0xff * 3);
 }
 
-function setColor(file, x, y, r = 0x00, g = 0x00, b = 0x00, a = 0xff) {
-  let idx = (file.width * y + x) << 2;
+// edge
+setValue(newfile, 0, 0, 1);
+setValue(newfile, max, 0, 0.5);
+setValue(newfile, max, max, 0);
+setValue(newfile, 0, max, 0.5);
 
-  file.data[idx] = r;
-  file.data[idx + 1] = g;
-  file.data[idx + 2] = b;
-  file.data[idx + 3] = a;
-}
+function divide(size) {
+  const half = size / 2;
+  var scale = 0.2;
+  if (half < 1) return;
 
-function hoge(x, power) {
-  const c = Math.pow(2, power - 1);
-  if (x === c) {
-    return 0;
-  } else if (x < c) {
-    return - x + Math.pow(2, power - 1);
-  } else if (x > c) {
-    return x - Math.pow(2, power - 1);
-  }
-}
-
-// center = 1
-const center = Math.pow(2, power - 1);
-setColor(newfile, center, center, 0xff, 0xff, 0xff);
-
-// edge = 0
-for (let x = 0; x < nx; x++) {
-  setColor(newfile, x, 0, 0, 0, 0);
-  setColor(newfile, x, ny, 0, 0, 0);
-}
-for (let y = 0; y < ny; y++) {
-  setColor(newfile, 0, y, 0, 0, 0);
-  setColor(newfile, nx, y, 0, 0, 0);
-}
-
-for (let p = 1; p < power; p++) {
-  const iStep = Math.pow(2, power - p);
-  const neighborDistance = iStep / 2;
-  for (let i = 0; i < Math.pow(2, p - 1); i++) {
-    const sideLength = Math.pow(2, power) - iStep * (2 * i - 1);
-    // top
-    for (let j = 0; j < sideLength; j++) {
-      const x = (iStep / 2) * (2 * i - 1);
+  for (let y = half; y < ny; y += size) {
+    for (let x = half; x < nx; x += size) {
+      const samples = [
+        getValue(newfile, x - half, y - half),
+        getValue(newfile, x + half, y - half),
+        getValue(newfile, x + half, y + half),
+        getValue(newfile, x - half, y + half)
+      ].filter(v => v !== -1);
+      const sum = samples.reduce((previous, current) => {
+        return previous + current;
+      }, 0);
+      const average = samples.length > 0 ? sum / samples.length : 0;
+      let value = average + getRandomArbitrary(-scale, scale);
+      if (value < 0) { value = 0 }
+      if (value > 1) { value = 1 }
+      setValue(newfile, x, y, value);
     }
-
-    const y = Math.pow(2, power - 1);
-    const x = Math.pow(2, power - p - 1) + i * iStep;
-    const refLeft = x - neighborDistance;
-    const refRight = x + neighborDistance;
-    const refLeftValue = getValue(newfile, refLeft, y);
-    const refRightValue = getValue(newfile, refRight, y);
-    let value = getRandomSign() * getRandomArbitrary(0, 0.2) + (refLeftValue + refRightValue) / 2;
-    if (value < 0) { value = 0 }
-    if (value > 1) { value = 1 }
-    const yHalfLength = hoge(x, power);
-    for (let j = 0; j < yHalfLength; j++) {
-      setValue(newfile, x, y + j, value);
-      setValue(newfile, x, y - j, value);
-    }
-    setValue(newfile, x, y, value);
   }
-  // const distance = Math.pow(2, power - p);
-  // const xyStart = Math.pow(2, power - p - 1);
-  // const xyEnd = xyStart + distance;
-  // for (let xy = xyStart; xy < xyEnd; xy++) {
-  //   setColor(newfile, xy, xyStart, 0x7f, 0x7f, 0x7f);
-  //   setColor(newfile, xy, xyEnd, 0x7f, 0x7f, 0x7f);
-  //   setColor(newfile, xyStart, xy, 0x7f, 0x7f, 0x7f);
-  //   setColor(newfile, xyEnd, xy, 0x7f, 0x7f, 0x7f);
-  // }
-
+  for (let y = 0; y <= ny; y += half) {
+    for (let x = (y + half) % size; x <= nx; x += size) {
+      const samples = [
+        getValue(newfile, x, y - half),
+        getValue(newfile, x + half, y),
+        getValue(newfile, x, y + half),
+        getValue(newfile, x - half, y)
+      ].filter(v => v !== -1);
+      const sum = samples.reduce((previous, current) => {
+        return previous + current;
+      }, 0);
+      const average = samples.length > 0 ? sum / samples.length : 0;
+      let value = average + getRandomArbitrary(-scale, scale);
+      if (value < 0) { value = 0 }
+      if (value > 1) { value = 1 }
+      setValue(newfile, x, y, value);
+    }
+  }
+  divide(size / 2);
 }
+
+divide(Math.pow(2, n));
 
 newfile
   .pack()
